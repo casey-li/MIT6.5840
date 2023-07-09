8755e73fba4c272807cafcfaebac05c588884922
casey-li <1063291196@qq.com>
2023-07-09 20:37:37 +0800

    提交Lab2实验结果，500次通过

--- Lab2/Lab2D/6.5840/src/raft/run_tests.sh
+++ Lab2/Lab2D/6.5840/src/raft/run_tests.sh
@@ -1,9 +1,9 @@
 # 运行脚本循环跑测试用例
 
-for ((i=1;i<=150;i++))
+for ((i=1;i<=500;i++))
 do 
-    result_file="./test_result/result.txt"
+    result_file="./test_result/test_2_500times.txt"
     echo -e "Running the $i tests iteration" >> $result_file
-    go test -run 2D -race >> $result_file
+    go test -run 2 -race >> $result_file
     echo " "  >> $result_file
 done
